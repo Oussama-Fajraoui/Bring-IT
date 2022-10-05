fffc08ca6ddd7b2a7caf9056ad5d23948cead391
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-10-05 11:49:44 +0100

    HomeScreen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,67 @@
+import { View, Text, SafeAreaView, StyleSheet, StatusBar, Image, TextInput  } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { UserIcon, ChevronDownIcon, SearchIcon, AdjustmentsIcon } from "react-native-heroicons/outline"
+
+
+const HomeScreen = () => {
+
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        });
+    },[]);
+
+
+  return (
+    <SafeAreaView className="bg-white pt-10">
+      <Text className="text-red-500">
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+            <Image 
+            source={{
+                uri: "https://links.papareact.com/wru"
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+            />
+            <View className="flex-1">
+                <Text className="font-bold text-gray-400 text-xs">
+                    Deliver Now !
+                    </Text>
+                <Text className="font-bold text-xl">
+                    Cureent Location
+                <ChevronDownIcon size={20} color="#00CCBB" />
+                </Text>
+            </View>
+
+            <UserIcon size={35} color="#00CCBB" />
+        </View>
+
+        {/* Search */}
+        <View>
+            <View>
+                <SearchIcon />
+                <TextInput 
+                placeholder='Restaurants and cuisines'
+                keyboardType='default'
+                />
+            </View>
+            <AdjustmentsIcon color="#00CCBB" />
+        </View>
+      </Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         marginTop:StatusBar.currentHeight,
+//         backgroundColor:"white",
+//         paddingTop: 5
+//     },
+// })
